41710c77e789a8e103e8b59c9e8f2cd3c862be11
far1ca <77165212+far1ca@users.noreply.github.com>
2025-10-20 14:44:23 +0200

    Fix images, start work on coin animations

--- nim-game/src/app/app.ts
+++ nim-game/src/app/app.ts
@@ -1,14 +1,42 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+interface Pile {
+  coinsArray: number[];
+  key: number;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('nim-game');
 
-  piles = [1, 5, 6, 1, 10];
+  N = 6; // number of piles
+  piles: Pile[] = [];
+  showInput = -1;
+
+  constructor() {
+    this.generatePiles();
+  }
+
+  generatePiles() {
+    this.piles = Array.from({ length: this.N }, (_, i) => ({
+      coinsArray: Array.from({ length: Math.floor(Math.random() * 10) + 1 }, () => 1),
+      key: i + 1,
+    }));
+    console.log(this.piles);
+  }
+
+  makeInputVisible(key: number) {
+    this.showInput = key;
+  }
+
+  removeCoins(pile: Pile, coinsToRemove: number) {
+    if (!coinsToRemove) return;
+    coinsToRemove = Math.min(coinsToRemove, pile.coinsArray.length);
+  }
 }
