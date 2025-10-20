0fefcffa9b08fbdf543f049277e6e21902e86aa2
far1ca <77165212+far1ca@users.noreply.github.com>
2025-10-20 15:11:56 +0200

    Adjust animations to work correctly, add provideAnimations()

--- nim-game/src/app/app.config.ts
+++ nim-game/src/app/app.config.ts
@@ -1,6 +1,11 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
@@ -8,6 +13,8 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
-  ]
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideAnimations(),
+  ],
 };
--- nim-game/src/app/app.ts
+++ nim-game/src/app/app.ts
@@ -21,6 +21,9 @@ interface Pile {
           style({
             transform: 'translateY(50px)',
             opacity: 0,
+            height: '0px', // parent collapses
+            width: '0px', // parent collapses
+            margin: '0px',
           })
         ),
       ]),
@@ -43,7 +46,6 @@ export class App {
       coinsArray: Array.from({ length: Math.floor(Math.random() * 10) + 1 }, () => 1),
       key: i + 1,
     }));
-    console.log(this.piles);
   }
 
   makeInputVisible(key: number) {
@@ -52,7 +54,17 @@ export class App {
 
   removeCoins(pile: Pile, coinsToRemove: number) {
     if (!coinsToRemove) return;
-    coinsToRemove = Math.min(coinsToRemove, pile.coinsArray.length);
-    pile.coinsArray.splice(pile.coinsArray.length - coinsToRemove, coinsToRemovey);
+    coinsToRemove = Math.min(coinsToRemove, pile.coinsArray.length + 1);
+    if (coinsToRemove == pile.coinsArray.length + 1) {
+      for (let i = 0; i < this.piles.length; i++) {
+        if (this.piles[i].key == pile.key) {
+          this.piles.splice(i, 1);
+          this.showInput = -1;
+          return;
+        }
+      }
+    }
+    pile.coinsArray.splice(pile.coinsArray.length - coinsToRemove, coinsToRemove);
+    this.showInput = -1;
   }
 }
